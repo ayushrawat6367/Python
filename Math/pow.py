8dab817280ace35d0d9781ffae62101649aa9e14
Ayush Rawat <ayushrawat531@gmail.com>
2024-10-05 12:33:53 -0400

    Pow(x, n)

---
+++ Math/pow.py
@@ -0,0 +1,37 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        # Base case for exponent 0
+        if n == 0:
+            return 1.0
+        
+        # Handle negative exponent by taking reciprocal
+        if n < 0:
+            x = 1 / x
+            n = -n
+        
+        result = 1.0
+        current_product = x
+        
+        while n > 0:
+            # If n is odd, multiply the result by current product
+            if n % 2 == 1:
+                result *= current_product
+            
+            # Square the current product for the next iteration
+            current_product *= current_product
+            n //= 2  # Divide n by 2 (integer division)
+        
+        return result
+
+
+x = 2.00000
+n = 10
+print(Solution().myPow(x, n))
+
+x = 2.10000
+n = 3
+print(Solution().myPow(x, n))
+
+x = 2.00000
+n = -2
+print(Solution().myPow(x, n))
